5b9678c786d1e65b8834aefc31915b8ff921ca3a
Danila <72153421+Danila459@users.noreply.github.com>
2023-05-10 13:40:08 +0400

    RSVP form

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+            {
+                guest.AddGuests(guest);
+                return View("Thanks", guest);
+            }
+            else
+
+                return View();
+        }
+
+        public ViewResult Guests()
+        {
+            Guests guests = new Guests();
+
+            return View(guests.GetGuests());
+            // return View();
+
+        }
+
+
+    }
+}
